4d5d2c149805f695e72ba03ca1363427b5ed3943
TerryH <terry@tnthughes.net>
2018-10-17 22:26:36 -0700

    Workman layer

--- keyboards/levinson/keymaps/TerryHughes/keymap.c
+++ keyboards/levinson/keymaps/TerryHughes/keymap.c
@@ -9,15 +9,21 @@ extern keymap_config_t keymap_config;
 #define _QWERTY   0
 #define _COLEMAK  1
 #define _DVORAK   2
-#define _LOWER    3
-#define _RAISE    4
-#define _ADJUST  16
+#define _WORKMAN  3
+#define _NUMBER  16
+#define _SYMBOL  17
+#define _LOWER   29
+#define _RAISE   30
+#define _ADJUST  31
 
 enum custom_keycodes
 {
     QWERTY = SAFE_RANGE,
     COLEMAK,
     DVORAK,
+    WORKMAN,
+    NUMBER,
+    SYMBOL,
     LOWER,
     RAISE,
     ADJUST,
@@ -84,6 +90,60 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     ADJUST ,KC_LCTL,KC_LALT,KC_LGUI,LOWER  ,KC_SPC ,        KC_SPC ,RAISE  ,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT  \
 ),
 
+/* Workman [https://github.com/ojbucao/Workman/]
+ * ,-----------------------------------------------.       ,-----------------------------------------------.
+ * |  Tab  |   Q   |   D   |   R   |   W   |   B   |       |   J   |   F   |   U   |   P   |   ;   | Bksp  |
+ * |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
+ * |  Esc  |   A   |   S   |   H   |   T   |   G   |       |   Y   |   N   |   E   |   O   |   I   |   '   |
+ * |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
+ * | Shift |   Z   |   X   |   M   |   C   |   V   |       |   K   |   L   |   ,   |   .   |   /   | Enter |
+ * |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
+ * |Adjust | Ctrl  |  Alt  |  GUI  |Number | Space |       | Space |Symbol |       | Menu  |       |  Del  |
+ * `-----------------------------------------------'       `-----------------------------------------------'
+ */
+[_WORKMAN] = LAYOUT_ortho_4x12( \
+    KC_TAB ,KC_Q   ,KC_D   ,KC_R   ,KC_W   ,KC_B   ,        KC_J   ,KC_F   ,KC_U   ,KC_P   ,KC_SCLN,KC_BSPC, \
+    KC_ESC ,KC_A   ,KC_S   ,KC_H   ,KC_T   ,KC_G   ,        KC_Y   ,KC_N   ,KC_E   ,KC_O   ,KC_I   ,KC_QUOT, \
+    KC_LSFT,KC_Z   ,KC_X   ,KC_M   ,KC_C   ,KC_V   ,        KC_K   ,KC_L   ,KC_COMM,KC_DOT ,KC_SLSH,KC_ENT , \
+    ADJUST ,KC_LCTL,KC_LALT,KC_LGUI,NUMBER ,KC_SPC ,        KC_SPC ,SYMBOL ,_______,KC_APP ,_______,KC_DEL   \
+),
+
+/* Number
+ * ,-----------------------------------------------.       ,-----------------------------------------------.
+ * |       |  F1   |  F2   |  F3   |  F4   |   A   |       |   B   |   7   |   8   |   9   |   *   |       |
+ * |-------+-------+-------+------+--------+-------|       |-------+-------+-------+-------+-------+-------|
+ * |       |  F5   |  F6   |  F7   |  F8   |   C   |       |   D   |   4   |   5   |   6   |   -   |       |
+ * |-------+-------+-------+------+--------+-------|       |-------+-------+-------+-------+-------+-------|
+ * |       |  F9   |  F10  |  F11  |  F12  |   E   |       |   F   |   1   |   2   |   3   |   +   |       |
+ * |-------+-------+-------+------+--------+-------|       |-------+-------+-------+-------+-------+-------|
+ * |       |       |       |       |       |       |       |       |   0   |   /   |   .   | Enter |       |
+ * `-----------------------------------------------'       `-----------------------------------------------'
+ */
+[_NUMBER] =  LAYOUT_ortho_4x12( \
+    _______,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_A   ,        KC_B   ,KC_P7  ,KC_P8  ,KC_P9  ,KC_PAST,_______, \
+    _______,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8  ,KC_C   ,        KC_D   ,KC_P4  ,KC_P5  ,KC_P6  ,KC_PMNS,_______, \
+    _______,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_E   ,        KC_F   ,KC_P1  ,KC_P2  ,KC_P3  ,KC_PPLS,_______, \
+    _______,_______,_______,_______,_______,_______,        _______,KC_P0  ,KC_PSLS,KC_PDOT,KC_PENT,_______  \
+),
+
+/* Symbol
+ * ,-----------------------------------------------.       ,-----------------------------------------------.
+ * |       |       |   _   |   ^   |   %   |       |       |       |   `   |   $   |   @   |       |       |
+ * |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
+ * |       |       |   |   |   #   |   !   |       |       |       |   =   |   *   |   &   |       |       |
+ * |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
+ * |       |   <   |   {   |   [   |   (   |   /   |       |   \   |   )   |   ]   |   }   |   >   |       |
+ * |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
+ * |       |       |       |       |       |       |       |       |       |       |       |       |       |
+ * `-----------------------------------------------'       `-----------------------------------------------'
+ */
+[_SYMBOL] =  LAYOUT_ortho_4x12( \
+    _______,_______,KC_UNDS,KC_CIRC,KC_PERC,_______,        _______,KC_GRV ,KC_DLR ,KC_AT  ,_______,_______, \
+    _______,_______,KC_PIPE,KC_HASH,KC_EXLM,_______,        _______,KC_EQL ,KC_ASTR,KC_AMPR,_______,_______, \
+    _______,KC_LABK,KC_LCBR,KC_LBRC,KC_LPRN,KC_SLSH,        KC_BSLS,KC_RPRN,KC_RBRC,KC_RCBR,KC_RABK,_______, \
+    _______,_______,_______,_______,_______,_______,        _______,_______,_______,_______,_______,_______  \
+),
+
 /* Lower
  * ,-----------------------------------------------.       ,-----------------------------------------------.
  * |   ~   |   !   |   @   |   #   |   $   |   %   |       |   ^   |   &   |   *   |   (   |   )   | Bksp  |
@@ -126,7 +186,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
  * |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
  * |       |       |       |Aud on |Aud off|AGnorm |       |AGswap |Qwerty |Colemak|Dvorak |       |       |
  * |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
- * |       |       |       |       |       |       |       |       |       |       |       |       |       |
+ * |       |       |       |       |       |       |       |       |Workman|       |       |       |       |
  * |-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
  * |       |       |       |       |       |       |       |       |       |       |       |       |       |
  * `-----------------------------------------------'       `-----------------------------------------------'
@@ -134,7 +194,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 [_ADJUST] =  LAYOUT_ortho_4x12( \
     _______,RESET  ,_______,_______,_______,_______,        _______,_______,_______,_______,_______,KC_DEL , \
     _______,_______,_______,AU_ON  ,AU_OFF ,AG_NORM,        AG_SWAP,QWERTY ,COLEMAK,DVORAK ,_______,_______, \
-    _______,_______,_______,_______,_______,_______,        _______,_______,_______,_______,_______,_______, \
+    _______,_______,_______,_______,_______,_______,        _______,WORKMAN,_______,_______,_______,_______, \
     _______,_______,_______,_______,_______,_______,        _______,_______,_______,_______,_______,_______  \
 )
 
@@ -144,6 +204,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 float tone_qwerty [][2] = SONG( QWERTY_SOUND);
 float tone_colemak[][2] = SONG(COLEMAK_SOUND);
 float tone_dvorak [][2] = SONG( DVORAK_SOUND);
+float tone_workman[][2] = SONG(WORKMAN_SOUND);
 
 #define PlaySong(Tone) PLAY_SONG(Tone)
 #else
@@ -163,6 +224,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
         case QWERTY:  { if (record->event.pressed) { PlaySong(tone_qwerty ); persistent_default_layer_set(1UL << _QWERTY ); } return false; } break;
         case COLEMAK: { if (record->event.pressed) { PlaySong(tone_colemak); persistent_default_layer_set(1UL << _COLEMAK); } return false; } break;
         case DVORAK:  { if (record->event.pressed) { PlaySong(tone_dvorak ); persistent_default_layer_set(1UL << _DVORAK ); } return false; } break;
+        case WORKMAN: { if (record->event.pressed) { PlaySong(tone_workman); persistent_default_layer_set(1UL << _WORKMAN); } return false; } break;
+
+        case NUMBER: { if (record->event.pressed) { layer_on (_NUMBER); }
+                       else                       { layer_off(_NUMBER); }                                                     return false; } break;
+        case SYMBOL: { if (record->event.pressed) { layer_on (_SYMBOL); }
+                       else                       { layer_off(_SYMBOL); }                                                     return false; } break;
 
         case LOWER: { if (record->event.pressed) { layer_on (_LOWER); update_tri_layer(_LOWER, _RAISE, _ADJUST); }
                       else                       { layer_off(_LOWER); update_tri_layer(_LOWER, _RAISE, _ADJUST); }            return false; } break;
